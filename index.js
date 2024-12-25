5f1d6d5b9676f12350ed6d002404c8b652032122
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-25 14:53:50 +0000

    Ticket 5 complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello from JS file")
+console.log("Hello from JS file");
 
 //Ticket1✅
 
@@ -33,8 +33,8 @@ console.log(userInput);
 //Run npx jest to see if you have passed ticket 3.✅
 
 if (userInput === correctPassword) {
-    alert("My favourite colour is #000080!")
-};
+  alert("My favourite colour is #000080!");
+}
 
 //Ticket 4 - Letting the user try again!
 
@@ -43,22 +43,80 @@ if (userInput === correctPassword) {
 // We can have the code loop around the part where we ask for the password using a loop.
 //Declare a variable called attempts with the value of 0✅
 //Use the variable attempts in the condition of the loop
-//If the user enters the wrong password, we want to add 1 to the attempts variable. 
+//If the user enters the wrong password, we want to add 1 to the attempts variable.
 //If they are correct, we want to stop looping.
 //Run `npx jest` to see if you have passed ticket 4.
 
-let attempts = 0;
+// let attempts = 0;
 
-while (attempts < 3) {
-    let userInput = prompt("Please enter the password.");
-    
-     if (userInput === correctPassword) {
-        alert("My favourite colour is #000080!")
-        break;
-    } else { 
-        attempts++
+// while (attempts < 3) {
+//   let userInput = prompt("Please enter the password.");
+
+//   if (userInput === correctPassword) {
+//     alert("My favourite colour is #000080!");
+//     break;
+//   } else {
+//     attempts++;
+//   }
+// }
+// if (attempts === 3) {
+//   alert("You have used all your attempts!");
+// }
+
+//  Ticket 5 - Functions
+// Declare a function that will prompt the user for input(a password)
+// Store that password in a variable within the function
+//   - Check whether the password matches the correct password (same as above):
+//     - If it does, return true from the function.
+//     - If it doesn't, let them try again.
+//     - If they fail three times in a row, do not allow them any more tries,
+//     -  and return false from the function.
+// - `console.log` the secret information (same as above) _only_ if the function returned true.
+
+// function userPassword() {
+//   let numberOfAttempts = 0;
+//   const correctPassword = "myPassword1!";
+
+//   while (numberOfAttempts < 2) {
+//     let userInputPassword = prompt("Please enter the password.");
+
+//     if (userInputPassword === correctPassword) {
+//       alert("My favourite colour is #000080!");
+//       return true;
+//     } else numberOfAttempts++;
+//   }
+
+//   return false;
+// }
+// if (userPassword()) {
+//   console.log("My favourite colour is #000080!");
+// } else {
+//   console.log("You have used up all attempts.");
+// }
+//  Refactor the code you have written into a function called `authenticateUser`.
+//Have that function return `true` if the user logged in successfully with the correct password
+//and `false` if they got the password wrong more than three times.
+//You can put most of the code you have already written in the previous steps into the function,
+// but don't reveal the secret yet. The function should be used like this:
+
+function authenticateUser() {
+  let numberOfAttempts = 0;
+  const correctPassword = "myPassword1!";
+
+  while (numberOfAttempts < 2) {
+    let userInputPassword = prompt("Please enter the password.");
+
+    if (userInputPassword === correctPassword) {
+      return true;
+    } else {
+      numberOfAttempts++;
     }
+  }
+  return false;
 }
- if (attempts === 3){
-    alert("You have used all your attempts!")
- }
+let isLoggedIn = authenticateUser();
+if (isLoggedIn === true) {
+  alert("My favourite colour is #000080!");
+}
+
+// - run `npx jest` to see if you have passed ticket 5.
